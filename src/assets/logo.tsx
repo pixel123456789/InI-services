b1deb6d4d7839a31bb3761f0f31b8823c46cb210
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2024-12-19 15:23:38 +0000

    Update logo.tsx

--- src/assets/logo.tsx
+++ src/assets/logo.tsx
@@ -3,8 +3,8 @@ function Logo(props: IconTypes) {
         <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 200 200"
-    width="200"
-    height="200">
+    width="100"
+    height="100">
     <circle cx="100" cy="100" r="80" fill="none" stroke="white" stroke-width="30" />
 </svg>
     )

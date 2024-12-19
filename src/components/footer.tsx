011df93c14ec268b7f2b38de3d6535dd8fa326e0
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2024-12-19 14:08:20 +0000

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,7 @@ function Footer() {
     return (
         <footer class="footer px-7 mt-auto mb-5 select-none flex items-center justify-between">
             <div>
-                <Obfuscated>© Metallic 2022</Obfuscated> - {new Date().getFullYear()}
-            </div>
-            <div class="flex items-center justify-center gap-1.5">
-                <a class="hover:underline" href={github}><Obfuscated>GitHub</Obfuscated></a>
-                <span>/</span>
-                <a class="hover:underline" href={discord}><Obfuscated>Discord</Obfuscated></a>
-                <span>/</span>
-                <a class="hover:underline" href="/privacy">Privacy</a>
+                <Obfuscated>Your Fucking Welcome Boys.</Obfuscated>
             </div>
         </footer>
     )

8576352258225f6cd8b99c0368e94aa938929542
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2025-01-06 17:30:31 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,31 +10,33 @@ import { Privacy } from "./pages/privacy";
 import { Error } from "./pages/error";
 import { Nav } from "./components/nav";
 import { Footer } from "./components/footer";
+import { Links } from "./pages/links";
 
 import "./util/locale";
 import "./style/index.css";
 
 function App() {
-	return (
-		<>
-			<ObfuscateLayout />
-			<Nav />
-			<main class="main p-7">
-				<Router>
-					<Route path="/" component={Home} />
-					<Route path="/apps" component={Apps} />
-					<Route path="/games" component={Games} />
-					<Route path="/settings/search" component={Settings} />
-					<Route path="/settings/tab" component={Settings} />
-					<Route path="/settings/appearance" component={Settings} />
-					<Route path="/settings/locale" component={Settings} />
-					<Route path="/privacy" component={Privacy} />
-					<Route default component={Error} />
-				</Router>
-			</main>
-			<Footer />
-		</>
-	);
+		return (
+				<>
+						<ObfuscateLayout />
+						<Nav />
+						<main class="main p-7">
+								<Router>
+										<Route path="/" component={Home} />
+										<Route path="/apps" component={Apps} />
+										<Route path="/games" component={Games} />
+										<Route path="/links" component={Links} />
+										<Route path="/settings/search" component={Settings} />
+										<Route path="/settings/tab" component={Settings} />
+										<Route path="/settings/appearance" component={Settings} />
+										<Route path="/settings/locale" component={Settings} />
+										<Route path="/privacy" component={Privacy} />
+										<Route default component={Error} />
+								</Router>
+						</main>
+						<Footer />
+				</>
+		);
 }
 
 render(<App />, document.getElementById("app")!);

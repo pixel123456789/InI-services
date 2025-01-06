f7868caebdd797b97cace612638c346ef8c95cf7
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2025-01-06 17:47:22 +0000

    Update nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,6 +5,7 @@ import { Logo } from '../assets/logo';
 import { HomeIcon } from "../assets/homeIcon";
 import { AppsIcon } from "../assets/appsIcon";
 import { GamesIcon } from "../assets/gamesIcon";
+import { LinksIcon } from "../assets/linkIcon.tsx";
 import { SettingsIcon } from "../assets/settingsIcon";
 import { Obfuscated } from '../util/obfuscate';
 
@@ -43,6 +44,7 @@ function Nav() {
                 </Link>
                 <Link href="/links">
                   <Button active={true}>
+                      <LinksIcon />
                     <span class="font-bold sr-only sm:not-sr-only">{t("Links")}</span>
                   </Button>
                 </Link>

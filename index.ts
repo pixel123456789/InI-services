8c9ab6af63a0afaf9d3f5a31d61f039d9d9a1711
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2025-01-07 00:47:53 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { ChemicalServer } from "chemicaljs";
-import { ChatServer } from "./src/util/websocket";
 import express from "express";
 import fs from "fs";
 import { execSync } from "child_process";
@@ -9,17 +8,14 @@ import { Request, Response } from "express";
 import pages from "./src/pages.json";
 import themes from "./src/themes.json";
 
-const [app, server] = new ChemicalServer();
-
-// Initialize WebSocket server
-new ChatServer(server);
+const [app, listen] = new ChemicalServer();
 
 const __dirname = path.resolve();
 
 if (!fs.existsSync("build")) {
-    console.log("No build found. Building...");
-    execSync("npm run build");
-    console.log("Built!");
+	console.log("No build found. Building...");
+	execSync("npm run build");
+	console.log("Built!");
 }
 
 const port = process.env.PORT || 3000;
@@ -29,33 +25,34 @@ app.use(express.static("build"));
 app.serveChemical();
 
 app.use((req: Request, res: Response) => {
-    if (pages.includes(req.url)) {
-        return res.sendFile(__dirname + "/build/index.html");
-    } else {
-        return res.status(404).sendFile(__dirname + "/build/index.html");
-    }
+	if (pages.includes(req.url)) {
+		return res.sendFile(__dirname + "/build/index.html");
+	} else {
+		return res.status(404).sendFile(__dirname + "/build/index.html");
+	}
 });
 
-server.listen(port, () => {
-    const theme = chalk.hex(
-        themes.filter((theme) => (theme.id = "default"))[0].theme.primary
-    );
-    console.log(`${chalk.bold(theme("Metallic"))}`);
-    console.log(`- ${chalk.bold("Local:")} http://localhost:${port}`);
+listen(port, () => {
+	const theme = chalk.hex(
+		themes.filter((theme) => (theme.id = "default"))[0].theme.primary
+	);
+	console.log(`${chalk.bold(theme("Metallic"))}`);
 
-    if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
-        console.log(
-            `- ${chalk.bold("Replit:")} https://${process.env.REPL_SLUG}.${
-                process.env.REPL_OWNER
-            }.repl.co`
-        );
-    }
+	console.log(`- ${chalk.bold("Local:")} http://localhost:${port}`);
 
-    if (process.env.HOSTNAME && process.env.GITPOD_WORKSPACE_CLUSTER_HOST) {
-        console.log(
-            `- ${chalk.bold("Gitpod:")} https://${port}-${process.env.HOSTNAME}.${
-                process.env.GITPOD_WORKSPACE_CLUSTER_HOST
-            }`
-        );
-    }
+	if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
+		console.log(
+			`- ${chalk.bold("Replit:")} https://${process.env.REPL_SLUG}.${
+				process.env.REPL_OWNER
+			}.repl.co`
+		);
+	}
+
+	if (process.env.HOSTNAME && process.env.GITPOD_WORKSPACE_CLUSTER_HOST) {
+		console.log(
+			`- ${chalk.bold("Gitpod:")} https://${port}-${process.env.HOSTNAME}.${
+				process.env.GITPOD_WORKSPACE_CLUSTER_HOST
+			}`
+		);
+	}
 });
